d7f9c305613cbcd9e08cb641b69a1d193fd24caa
Lamontthedev <83934672+Lamontthedev@users.noreply.github.com>
2021-08-26 17:56:29 -0400

    Updated the script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,32 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  
+  {
+    quote: 'Intelligence plus character, that is the goal of true education.',
+    source: 'Martin Luther King Jr.',
+  },
+  {
+    quote: 'It always seems impossible until it is done.',
+    source: 'Nelson Mandela',
+  },
+  {
+    quote: 'When you learn, teach. When you get, give',
+    source: 'Maya Angelou', 
+    citation: '@InspiringThinkn. Motivational-Inspirational-World.Blogspot.com'
+  },
+  {
+    quote: 'Education is our passport to the future, for tomorrow belongs to the people who prepare for it today.',
+    source: 'Malcom X',
+  },
+  {
+    quote: 'If there is no struggle, there is no progress.',
+    source: 'Frederick Douglass',
+    citation: 'Frederick Douglass Biography: From Slave to Abolitionist.'
+  }
 
+];
 
 /***
  * `getRandomQuote` function
